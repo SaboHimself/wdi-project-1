92461efab3cf9a5b7bbfe15d46d92a123379f32a
Stephen Sabo <sabo.stephen@gmail.com>
2019-02-18 21:32:33 +0000

    Working on logic to players ship placement

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Create Ship constructor
-
   class Ship {
     constructor(type, size, id) {
       this.type = type
@@ -13,15 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Create board - default empty board one for each player
-
   class Board {
     constructor (playerType) {
       this.playerType = playerType
       this.hits = []
       this.grid = []
+      this.buildReferenceGrid()
+      this.buildBoard()
+      this.buildFleet()
+      this.placeCpuShips()
     }
 
-    buildBoard() {
+    buildReferenceGrid() {
       for(let i = 0; i < 10; i++) {
         this.grid.push([])
         for(let j = 0; j < 10; j++) {
@@ -30,101 +32,84 @@ document.addEventListener('DOMContentLoaded', () => {
       } //end of first for loop
       return this.grid
     }
-  }
 
-  // Create a fleet function to store all ships in a list
-  function buildFleet() {
-    const carrier = new Ship('carrier', 5, 'AC')
-    const battleship = new Ship('battleship', 4, 'B')
-    const cruiser = new Ship('cruiser', 3, 'C')
-    const submarine = new Ship('submarine', 3, 'S')
-    const destroyer = new Ship('destroyer', 2, 'D')
+    buildBoard() {
+      if(this.playerType === 'cpu') {
+        this.board = document.querySelector('.cpu-board')
+      } else {
+        this.board = document.querySelector('.player-board')
+      }
+      for(let i = 0; i < 100; i++) {
+        this.square = document.createElement('div')
+        this.square.setAttribute('class', 'square')
+        this.square.setAttribute('data-id', i)
+        this.board.appendChild(this.square)
+      }
+      const divs = document.querySelectorAll('.square')
+      divs.forEach(div => div.addEventListener('click', this.placePlayerShips.bind(this)))
+    }
 
-    const fleet = [carrier, battleship, cruiser, submarine, destroyer]
-    return fleet
+    buildFleet () {
+      this.fleet = [
+        new Ship('carrier', 5, 'AC'),
+        new Ship('battleship', 4, 'B'),
+        new Ship('cruiser', 3, 'C'),
+        new Ship('submarine', 3, 'S'),
+        new Ship('destroyer', 2, 'D')
+      ]
+    }
+
+    placeCpuShips() {
+      let ship
+      let xCoord
+      let yCoord
+
+      if(this.playerType === 'cpu') {
+
+        for(let i = 0; i < this.fleet.length; i++) {
+          const direction = Math.floor(Math.random() * 2) // 0 for horizontal. 1 for vertical
+          ship = this.fleet[i]
+          ship.direction = direction
+
+          if(direction === 0) {
+            xCoord = Math.floor(Math.random() * (10 - ship.size))
+            yCoord = Math.floor(Math.random() * 10)
+          } else {
+            xCoord = Math.floor(Math.random() * 10)
+            yCoord = Math.floor(Math.random() * (10 - ship.size))
+          }
+          ship.coordinates.push(xCoord, yCoord)
+
+          for(let j = 0; j < ship.size; j++) {
+            if(direction === 0) {
+              this.grid[xCoord + j][yCoord] = ship.id
+            } else {
+              this.grid[xCoord][yCoord + j] = ship.id
+            }
+          }
+        }
+        return this.grid
+      }
+    }
+
+    placePlayerShips(e) {
+      console.log(e.target.getAttribute('data-id'))
+      if(this.playerType === 'player') {
+        for(let i = 0; i < this.fleet.length; i++) {
+          const currentShip = this.fleet[i]
+        }
+      }
+    }
   }
 
   const playerBoard = new Board('player')
   const cpuBoard = new Board('cpu')
-  const playerFleet = buildFleet()
-  const cpuFleet = buildFleet()
 
-  // Place cpu ships randomly within their board limits (will need a loop to keep checking if ships fit)
 
-  function placeCpuShips(fleet, board) {
-    let ship
-    let xCoord
-    let yCoord
-
-    for(let i = 0; i < fleet.length; i++) {
-      const direction = Math.floor(Math.random() * 2) // 0 for horizontal. 1 for vertical
-      ship = fleet[i]
-      ship.direction = direction
-
-      if(direction === 0) {
-        xCoord = Math.floor(Math.random() * (10 - ship.size))
-        yCoord = Math.floor(Math.random() * 10)
-      } else {
-        xCoord = Math.floor(Math.random() * 10)
-        yCoord = Math.floor(Math.random() * (10 - ship.size))
-      }
-      ship.coordinates.push(xCoord, yCoord)
-
-      for(let j = 0; j < ship.size; j++) {
-        if(direction === 0) {
-          board[xCoord + j][yCoord] = ship.id
-        } else {
-          board[xCoord][yCoord + j] = ship.id
-        }
-      }
-    }
-    return board
-  }
-
-  placeCpuShips(cpuFleet, cpuBoard.buildBoard())
-
-  // Create function to check for collisions
-
-  // Create grid of divs for each board
-  function buildPlayerBoard() {
-    const playerBoard = document.querySelector('.player-board')
-    for(let i = 0; i < 10; i++) {
-      const xSquare = document.createElement('div')
-      xSquare.setAttribute('class', 'square')
-      playerBoard.appendChild(xSquare)
-      for(let j = 1; j < 10; j++) {
-        const ySquare = document.createElement('div')
-        ySquare.setAttribute('class', 'square')
-        playerBoard.appendChild(ySquare)
-      }
-    }
-  }
-let cpuReferenceBoard = []
-
-  function buildCpuBoard() {
-    const cpuBoard = document.querySelector('.cpu-board')
-    for(let i = 0; i < 10; i++) {
-      const xSquare = document.createElement('div')
-      xSquare.setAttribute('class', 'square')
-      cpuBoard.appendChild(xSquare)
-      for(let j = 1; j < 10; j++) {
-        const ySquare = document.createElement('div')
-        ySquare.setAttribute('class', 'square')
-        cpuBoard.appendChild(ySquare)
-        cpuReferenceBoard.push()
-      }
-    }
-  }
-  buildPlayerBoard()
-  buildCpuBoard()
+  console.log(playerBoard)
+  console.log(cpuBoard)
 
   // Place players ships (check for collisions)
-
-  function placePlayerShips (fleet, board) {
-
-  }
-
-  placePlayerShips(playerFleet, playerBoard)
 
   // Game Start
 
@@ -135,7 +120,4 @@ let cpuReferenceBoard = []
   // Repeat above until either the player or cpu have no ships left in list
 
   //
-
-  console.log(cpuFleet)
-  console.log(cpuBoard)
 })
