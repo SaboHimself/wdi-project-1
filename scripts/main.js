45f252c7970bd8145bf18433025717fef8fd5044
Stephen Sabo <sabo.stephen@gmail.com>
2019-02-18 10:51:51 +0000

    Add grid build functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,95 +1,131 @@
-// Create Ship constructor
+document.addEventListener('DOMContentLoaded', () => {
+  // Create Ship constructor
 
-class Ship {
-  constructor(type, size, id) {
-    this.type = type
-    this.size = size
-    this.id = id
-    this.coordinates = []
-    this.direction
-    this.hits = 0
-  }
-}
-
-// Create board - default empty board one for each player
-function board() {
-  const grid = []
-
-  for(let i = 0; i < 10; i++) {
-    grid.push([])
-    for(let j = 0; j < 10; j++) {
-      grid[i].push(0)
+  class Ship {
+    constructor(type, size, id) {
+      this.type = type
+      this.size = size
+      this.id = id
+      this.coordinates = []
+      this.direction
+      this.hits = 0
     }
   }
-  return grid
-}
 
-// Create a fleet function to store all ships in a list
-function buildFleet() {
-  const carrier = new Ship('carrier', 5, 'AC')
-  const battleship = new Ship('battleship', 4, 'B')
-  const cruiser = new Ship('cruiser', 3, 'C')
-  const submarine = new Ship('submarine', 3, 'S')
-  const destroyer = new Ship('destroyer', 2, 'D')
+  // Create board - default empty board one for each player
+  function board() {
+    const grid = []
 
-  const fleet = [carrier, battleship, cruiser, submarine, destroyer]
-  return fleet
-}
+    for(let i = 0; i < 10; i++) {
+      grid.push([])
+      for(let j = 0; j < 10; j++) {
+        grid[i].push(0)
+      } // end of second for loop
+    } //end of first for loop
+    return grid
+  }
 
-const playerBoard = board()
-const cpuBoard = board()
-const playerFleet = buildFleet()
-const cpuFleet = buildFleet()
+  // Create a fleet function to store all ships in a list
+  function buildFleet() {
+    const carrier = new Ship('carrier', 5, 'AC')
+    const battleship = new Ship('battleship', 4, 'B')
+    const cruiser = new Ship('cruiser', 3, 'C')
+    const submarine = new Ship('submarine', 3, 'S')
+    const destroyer = new Ship('destroyer', 2, 'D')
 
-// Place cpu ships randomly within their board limits (will need a loop to keep checking if ships fit)
+    const fleet = [carrier, battleship, cruiser, submarine, destroyer]
+    return fleet
+  }
 
-function placeCpuShips(fleet, board) {
-  let ship
-  let xCoord
-  let yCoord
+  const playerBoard = board()
+  const cpuBoard = board()
+  const playerFleet = buildFleet()
+  const cpuFleet = buildFleet()
 
-  for(let i = 0; i < fleet.length; i++) {
-    const direction = Math.floor(Math.random() * 2) // 0 for horizontal. 1 for vertical
-    ship = fleet[i]
-    ship.direction = direction
+  // Place cpu ships randomly within their board limits (will need a loop to keep checking if ships fit)
 
-    if(direction === 0) {
-      xCoord = Math.floor(Math.random() * (10 - ship.size))
-      yCoord = Math.floor(Math.random() * 10)
-    } else {
-      xCoord = Math.floor(Math.random() * 10)
-      yCoord = Math.floor(Math.random() * (10 - ship.size))
-    }
+  function placeCpuShips(fleet, board) {
+    let ship
+    let xCoord
+    let yCoord
 
-    for(let j = 0; j < ship.size; j++) {
+    for(let i = 0; i < fleet.length; i++) {
+      const direction = Math.floor(Math.random() * 2) // 0 for horizontal. 1 for vertical
+      ship = fleet[i]
+      ship.direction = direction
+
       if(direction === 0) {
-        board[xCoord + j][yCoord] = ship.id
+        xCoord = Math.floor(Math.random() * (10 - ship.size))
+        yCoord = Math.floor(Math.random() * 10)
+      } else {
+        xCoord = Math.floor(Math.random() * 10)
+        yCoord = Math.floor(Math.random() * (10 - ship.size))
       }
-      if(direction === 1) {
-        board[xCoord][yCoord + j] = ship.id
+      ship.coordinates.push(xCoord, yCoord)
+
+      for(let j = 0; j < ship.size; j++) {
+        if(direction === 0) {
+          board[xCoord + j][yCoord] = ship.id
+        } else {
+          board[xCoord][yCoord + j] = ship.id
+        }
       }
     }
-    // board[xCoord][yCoord] = ship.id
-
-    //ARRAY.FILL()
+    return board
   }
-}
 
-placeCpuShips(cpuFleet, cpuBoard)
+  placeCpuShips(cpuFleet, cpuBoard)
 
-// Create function to check for collisions
+  // Create function to check for collisions
 
-// Place players ships (check for collisions)
+  // Create grid of divs for each board
+  function buildPlayerBoard() {
+    const playerBoard = document.querySelector('.player-board')
+    for(let i = 0; i < 10; i++) {
+      const xSquare = document.createElement('div')
+      xSquare.setAttribute('class', 'square')
+      playerBoard.appendChild(xSquare)
+      for(let j = 1; j < 10; j++) {
+        const ySquare = document.createElement('div')
+        ySquare.setAttribute('class', 'square')
+        playerBoard.appendChild(ySquare)
+      }
+    }
+  }
+  function buildCpuBoard() {
+    const cpuBoard = document.querySelector('.cpu-board')
+    for(let i = 0; i < 10; i++) {
+      const xSquare = document.createElement('div')
+      xSquare.setAttribute('class', 'square')
+      cpuBoard.appendChild(xSquare)
+      for(let j = 1; j < 10; j++) {
+        const ySquare = document.createElement('div')
+        ySquare.setAttribute('class', 'square')
+        cpuBoard.appendChild(ySquare)
+      }
+    }
+  }
+  buildPlayerBoard()
+  buildCpuBoard()
 
-// Game Start
+  // Place players ships (check for collisions)
 
-// Player attacks the cpu board - check for hit
+  function placePlayerShips (fleet, board) {
+    
+  }
 
-// Cpu attacks player board - check for hit
+  placePlayerShips(playerFleet, playerBoard)
 
-// Repeat above until either the player or cpu have no ships left in list
+  // Game Start
 
-//
+  // Player attacks the cpu board - check for hit
 
-console.log(cpuFleet)
-console.log(cpuBoard)
+  // Cpu attacks player board - check for hit
+
+  // Repeat above until either the player or cpu have no ships left in list
+
+  //
+
+  console.log(cpuFleet)
+  console.log(cpuBoard)
+})
